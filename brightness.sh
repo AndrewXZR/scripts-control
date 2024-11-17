eb3cb30c5cb86c48e14e086fcb88706c6de14da9
Andrew <andres.ferra@mi.unc.edu>
2024-11-17 23:57:13 +0000

    script implementation to control brightness

---
+++ brightness.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Path to the brightness file
+BRIGHTNESS_FILE="/sys/class/backlight/intel_backlight/brightness"
+
+# Getting maximum brightness
+MAX_BRIGHTNESS=$(cat /sys/class/backlight/intel_backlight/max_brightness)
+
+# Brightness adjustment steps
+STEP=1000
+
+# Validate entry
+if [[ "$1" != "up" && "$1" != "down" ]]; then
+    echo "Uso: $0 [up|down]"
+    exit 1
+fi
+
+# Get current brightness
+CURRENT_BRIGHTNESS=$(cat "$BRIGHTNESS_FILE")
+
+# Calculate the new brightness
+if [[ "$1" == "up" ]]; then
+    NEW_BRIGHTNESS=$((CURRENT_BRIGHTNESS + STEP))
+else
+    NEW_BRIGHTNESS=$((CURRENT_BRIGHTNESS - STEP))
+fi
+
+# Limit brightness between 0 and MAX_BRIGHTNESS
+NEW_BRIGHTNESS=$(($NEW_BRIGHTNESS < 0 ? 0 : $NEW_BRIGHTNESS))
+NEW_BRIGHTNESS=$(($NEW_BRIGHTNESS > $MAX_BRIGHTNESS ? $MAX_BRIGHTNESS : $NEW_BRIGHTNESS))
+
+# Apply the new gloss
+echo $NEW_BRIGHTNESS | sudo tee "$BRIGHTNESS_FILE" > /dev/null
+
+# Notify the change
+notify-send -t 3000 -i display-brightness "Brightness" "Adjusted to $((NEW_BRIGHTNESS * 100 / MAX_BRIGHTNESS))%"
