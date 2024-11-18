d6a3e4b76aebe8524ed46607438204aac4afd26a
Andrew <andres.ferra@mi.unc.edu>
2024-11-18 02:04:06 +0000

    #bug 2024-01 Avoid using permissions that may compromise the system

--- brightness.sh
+++ brightness.sh
@@ -6,31 +6,18 @@ if [[ "$1" != "up" && "$1" != "down" ]]; then
     exit 1
 fi
 
-# Path to the brightness file
-BRIGHTNESS_FILE="/sys/class/backlight/intel_backlight/brightness"
-
-# Getting maximum brightness
-MAX_BRIGHTNESS=$(cat /sys/class/backlight/intel_backlight/max_brightness)
-
 # Brightness adjustment steps
-STEP=1000
+STEP=10
 
 # Get current brightness
 CURRENT_BRIGHTNESS=$(cat "$BRIGHTNESS_FILE")
 
-# Calculate the new brightness
+# new brightness
 if [[ "$1" == "up" ]]; then
-    NEW_BRIGHTNESS=$((CURRENT_BRIGHTNESS + STEP))
+    light -A $STEP
 else
-    NEW_BRIGHTNESS=$((CURRENT_BRIGHTNESS - STEP))
+    light -U $STEP
 fi
 
-# Limit brightness between 0 and MAX_BRIGHTNESS
-NEW_BRIGHTNESS=$(($NEW_BRIGHTNESS < 0 ? 0 : $NEW_BRIGHTNESS))
-NEW_BRIGHTNESS=$(($NEW_BRIGHTNESS > $MAX_BRIGHTNESS ? $MAX_BRIGHTNESS : $NEW_BRIGHTNESS))
-
-# Apply the new gloss
-echo $NEW_BRIGHTNESS | sudo tee "$BRIGHTNESS_FILE" > /dev/null
-
 # Notify the change
-notify-send -t 3000 --replace-id=1 -i display-brightness "Brightness" "Adjusted to $((NEW_BRIGHTNESS * 100 / MAX_BRIGHTNESS))%"
+notify-send -t 3000 --replace-id=1 -i display-brightness "Brightness" "Adjusted to $(light -G | xargs printf "%.0f\n")%"
